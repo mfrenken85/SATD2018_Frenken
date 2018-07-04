e9d12b3b682f42d13dcf6667b1b0ee5b71318950
Mark Frenken <mfrenken85@gmail.com>
2018-07-04 17:33:31 +0200

    Version 1.0
    Everything seems to be working, ready for first release. Readme and documentation are not done yet.

---
+++ README.md
@@ -0,0 +1,7 @@
+# SATD2018
+CAPITA project by Mark Frenken on SATD
+
+## TODO
+- Finish this readme
+- Include the serialized classifier and the script to classify extracted comments.
+- Make it easy to use! have the classifier generate its files in new maps each time.
---
+++ classify_comments.py
@@ -0,0 +1,78 @@
+import os
+import shutil
+import sys
+import psycopg2
+import subprocess
+from subprocess import Popen, PIPE
+
+# This class can be used to classify comments using a serialized classifier.
+
+# When using serialized_classifier_tenfold.ser.gz, query a.treated_commenttext
+# When using serialized_classifier.ser.gz, query a.commenttext
+
+loc_classifier = os.path.expanduser("classifier/")
+serialized_classifier = "serialized_classifier_tenfold.ser.gz" #_tenfold
+
+password = "111111"#sys.argv[1]
+username = 'postgres'
+dbname = 'postgres'
+
+execution_options = {1: {'DOCUMENTATION', 'DESIGN', 'DEFECT', 'IMPLEMENTATION', 'TEST', 'WITHOUT_CLASSIFICATION'},
+                     2: {'DEFECT', 'WITHOUT_CLASSIFICATION'},
+                     3: {'DESIGN', 'WITHOUT_CLASSIFICATION'},
+                     4: {'DOCUMENTATION', 'WITHOUT_CLASSIFICATION'},
+                     5: {'IMPLEMENTATION', 'WITHOUT_CLASSIFICATION'},
+                     6: {'TEST', 'WITHOUT_CLASSIFICATION'},
+                     7: {'UNKNOWN'}
+}
+project = "fp2-launcher"  #fp2-launcher
+
+# create the files used by the stanford classifier, train and test both.
+def write_classifier_file(file_name, result):
+    with open (file_name,'a') as classified_seq:
+        for line in result:
+            classified_seq.write("{0}\t{1}\t{2}\n".format(line[0], line[1].replace('\n','').replace('\r\n', '').replace('\r', ''), line[2]))
+        classified_seq.close()
+
+def write_output_file(file_name, output):
+    with open (file_name,'a') as classified_seq:
+        for line in output:
+            classified_seq.write(line)
+        classified_seq.close()
+
+try:
+    connection = None
+
+    # connect to the database
+    connection = psycopg2.connect(host='localhost', port='5432', database=dbname, user=username, password=password)
+    cursor = connection.cursor()
+
+    # Remove previous output files.
+    subprocess.call(["rm", loc_classifier + "output/output.txt"])
+    subprocess.call(["rm", loc_classifier + "output/results.txt"])
+    subprocess.call(["rm", loc_classifier + "output/classified_seq.test"])
+
+    for td_type in execution_options[7]: # For now just query UNKNOWN
+        cursor.execute(
+            "select a.classification, a.treated_commenttext, a.id from processed_comment a, comment_class b "
+            "where a.commentclassid = b.id  and b.projectname like '%" + project + "%' and a.classification = '"+td_type+"'")
+        write_classifier_file(loc_classifier + "output/classified_seq.test", cursor.fetchall())
+
+    print("Analysis started for " + project)
+    output = subprocess.Popen([
+        "java -jar " + loc_classifier + "stanford-classifier.jar -loadClassifier " +
+        loc_classifier + serialized_classifier + " -testFile " +
+        loc_classifier + "output/classified_seq.test"],
+        stdout=PIPE, stderr=PIPE, shell=True).communicate()
+    #serialized_classifier.ser.gz
+
+    print("Analysis finished")
+    write_output_file(loc_classifier + "output/output.txt", output[0].strip().decode("utf-8"))
+    write_output_file(loc_classifier + "output/results.txt", output[1].strip().decode("utf-8"))
+
+
+
+
+except Exception as e:
+    print(e)
+    raise e
